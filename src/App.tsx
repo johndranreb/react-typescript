9b5521f8ea9650c08139e0a66f8f0cd68745967d
johndranreb <jhon.antero93@gmail.com>
2023-09-21 19:49:08 +0800

    added person.tsx and PersonList.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,35 @@
 import './App.css';
 import { Greet } from './components/Greet';
+import { PersonList } from './components/PersonList';
 
 function App() {
+  const personName = {
+    first: 'John',
+    last: 'Dranreb',
+  }
+
+  const personList =[
+    {
+      first: 'John',
+      last: 'Dranreb',
+    },
+    {
+      first: 'John',
+      last: 'Bernard',
+    },
+    {
+      first: 'J',
+      last: 'B',
+    },
+  ]
+
   return (
-    <div className="App">
-     <Greet name='John'/>
+    <div className='App'>
+     <Greet name='John' messageCount={20} isLoggedIn={false}/>
+     <Person name={personName} />
+     <PersonList names={nameList}
     </div>
-  );
+  )
 }
 
 export default App;
